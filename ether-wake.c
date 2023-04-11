c7cc041a51a1161e497ff078f459a2000e2a9555
Emily Ehlert <ehlertto@hu-berlin.de>
2023-04-11 21:46:48 +0200

    Implement hold functionality

--- ether-wake.c
+++ ether-wake.c
@@ -23,6 +23,7 @@ static char usage_msg[] =
 "		-b	Send wake-up packet to the broadcast address.\n"
 "		-D	Increase the debug level.\n"
 "		-i ifname	Use interface IFNAME instead of the default 'eth0'.\n"
+"		-h ipaddress	Hold packages back until host with ip address is reachable.\n"
 "		-p <pw>		Append the four or six byte password PW to the packet.\n"
 "					A password is only required for a few adapter types.\n"
 "					The password may be specified in ethernet hex format\n"
@@ -80,8 +81,12 @@ static char usage_msg[] =
 #include <netpacket/packet.h>
 #include <net/ethernet.h>
 #include <netinet/ether.h>
+#include <time.h>
+#include <sys/wait.h>
 
 #include "nfqueue.h"
+
+#define TIMEOUT 10
 
 u_char outpack[1000];
 int pktsize;
@@ -97,6 +102,9 @@ int debug = 0;
 u_char wol_passwd[6];
 int wol_passwd_sz = 0;
 
+static int hold = 0;
+static char **ping_argv;
+
 static int opt_no_src_addr = 0, opt_broadcast = 0;
 static int opt_nfqueue_num = -1;
 
@@ -105,10 +113,12 @@ static int get_dest_addr(const char *arg, struct ether_addr *eaddr);
 static int get_fill(unsigned char *pkt, struct ether_addr *eaddr);
 static int get_wol_pw(const char *optarg);
 static int get_nfqueue_num(const char *optarg);
+static void generate_ping_argv(const char *ip_address, const char *ifname);
 
 int main(int argc, char *argv[])
 {
 	char *ifname = "eth0";
+	char *ip_address;
 	int one = 1;				/* True, for socket options. */
 	int errflag = 0, nfqueue_errflag = 0, verbose = 0, do_version = 0;
 	int perm_failure = 0;
@@ -120,6 +130,7 @@ int main(int argc, char *argv[])
 		case 'b': opt_broadcast++;	break;
 		case 'D': debug++;			break;
 		case 'i': ifname = optarg;	break;
+		case 'h': hold++; ip_address = optarg; break;
 		case 'p': get_wol_pw(optarg); break;
 		case 'q':
 			if (get_nfqueue_num(optarg) < 0)
@@ -210,6 +221,10 @@ int main(int argc, char *argv[])
 		printf(".\n");
 	}
 
+	if (hold) {
+		generate_ping_argv(ip_address, ifname);
+	}
+
 	/* This is necessary for broadcasts to work */
 	if (setsockopt(s, SOL_SOCKET, SO_BROADCAST, (char *)&one, sizeof(one)) < 0)
 		perror("setsockopt: SO_BROADCAST");
@@ -244,6 +259,51 @@ int main(int argc, char *argv[])
 		printf("Acting on packets in NFQUEUE %d\n", opt_nfqueue_num);
 	return nfqueue_receive(opt_nfqueue_num, send_magic_packet);
 }
+void generate_ping_argv(const char *ip_address, const char *ifname)
+{
+	const char *ping_arguments[] = {
+		"/bin/ping", "-c", "1", "-W", "1", "-I", ifname, ip_address
+	};
+	const int argument_count =
+		sizeof(ping_arguments) / sizeof(ping_arguments[0]);
+	ping_argv = calloc(argument_count + 1, sizeof(char *));
+	if (!ping_argv) {
+		perror("malloc() ping arguments");
+		exit(EXIT_FAILURE);
+	}
+
+	for (int array_index = 0; array_index < argument_count; array_index++) {
+		char *str_cpy = strdup(ping_arguments[array_index]);
+		if (str_cpy) {
+			ping_argv[array_index] = str_cpy;
+		} else {
+			perror("malloc() ping argument");
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+static int execute_ping()
+{
+	pid_t ping_pid;
+	int status;
+
+	ping_pid = fork();
+	if (ping_pid == 0) {
+		fclose(stdout);
+		fclose(stdin);
+		// Must include some PATH for busybox on OpenWRT
+		char *envp[] = { "PATH=/bin:/sbin:/usr/bin:/usr/sbin", NULL };
+		execve("/bin/ping", ping_argv, envp);
+
+		perror("ping process execve failed [%m]");
+		return EXIT_FAILURE;
+	}
+
+	waitpid(ping_pid, &status, 0);
+
+	return WEXITSTATUS(status);
+}
 
 static int send_magic_packet()
 {
@@ -277,6 +337,24 @@ static int send_magic_packet()
 			printf("sendmsg worked, %d (%d).\n", i, errno);
 	}
 #endif
+
+	// Wait until host is reachable again
+	if (hold) {
+		static time_t last_time = -1;
+		time_t current_time = time(NULL);
+		int recent = current_time != -1 &&
+			     difftime(current_time, last_time) < 60;
+		if (!recent) {
+			int ping_ret = -1;
+			for (int ping_count = 0;
+			     ping_count < TIMEOUT && ping_ret != 0;
+			     ping_count++) {
+				ping_ret = execute_ping();
+			}
+		}
+
+		last_time = current_time;
+	}
 
 	return 0;
 }
